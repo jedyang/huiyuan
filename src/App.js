041523c842b993c52799fc49e2518dc9d2d404f1
杨云胜 <yangyunsheng@haier.com>
2017-12-16 17:36:09 +0800

    init2

--- src/App.js
+++ src/App.js
@@ -1,21 +1,27 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
+import { Layout, notification, Icon } from 'antd';
+import SiderCustom from './components/SiderCustom';
+import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+        <Layout>
+            <SiderCustom />
+        </Layout>
     );
   }
 }
 
-export default App;
+const mapStateToProps = state => {
+    const { auth = {data: {}}, responsive = {data: {}} } = state.httpData;
+    return {auth, responsive};
+};
+const mapDispatchToProps = dispatch => ({
+    receiveData: bindActionCreators(receiveData, dispatch)
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(App);
